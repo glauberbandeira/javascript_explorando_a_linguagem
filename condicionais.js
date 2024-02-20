451783e07c66dab37fa8911daf09b9c39239f814
glauberbandeira <glauberbandeira_@hotmail.com>
2024-02-20 16:31:29 +1100

    if_encadeado

--- condicionais.js
+++ condicionais.js
@@ -7,6 +7,8 @@ const listaDeDestino = new Array(
 );
 
 const idadeComprador = 17;
+const estaAcompanhada = true;
+
 console.log("Destinos possiveis;");
 console.log(listaDeDestino); // Imprimir uma lista
 
@@ -14,7 +16,13 @@ if(idadeComprador > 18) {
     console.log(`Maior de idade, pacote vendido`)
     listaDeDestino.splice(1, 1) // remover item
 } else {
-    console.log("Menor de idade nao pode comprar")
+    // A pessoa e menor de idade
+    if(estaAcompanhada) {
+        console.log(`O comprador esta acompanhado`)
+        listaDeDestino.splice(1, 1) // remover item
+    } else {
+        console.log("Menor de idade nao pode comprar")
+    }
 }
 console.log(listaDeDestino);
 
